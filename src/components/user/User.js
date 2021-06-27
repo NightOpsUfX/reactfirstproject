48be31c4161f49665b556f6903626afd8d4bdf4a
NightOpsUfx <leonoho17@gmail.com>
2021-06-27 15:32:36 +0300

    React

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,24 +1,40 @@
-export default function User({item}) {
-    return(
-        <div>
-            <h2>{item.name}</h2>
-            <div>
-                <p>User Login: {item.username}</p>
-                <p>User ID: {item.id}</p>
-                <p>User Company {item.company}</p>
-                <p>User Email {item.email}</p>
-                <p>User Phone {item.phone}</p>
-                <p>User Web Site {item.website}</p>
-            </div>
-        </div>
-    )
-}
-///////////
-
-// export default function User ({item}) {
-//     return (
+// export default function User({item: {name, username, id, company, email, phone, website}}) {
+//     return(
 //         <div>
-//             {item.name}
+//             <h2>{name}</h2>
+//             <div>
+//                 <p>User Login: {username}</p>
+//                 <p>User ID: {id}</p>
+//                 <p>User Company {company.name}</p>
+//                 <p>User Email {email}</p>
+//                 <p>User Phone {phone}</p>
+//                 <p>User Web Site {website}</p>
+//             </div>
 //         </div>
 //     )
 // }
+///////////////////////////////////
+
+import {useEffect, useState} from "react";
+import Posts from "../posts/Posts";
+
+export default function User ({item}) {
+
+    let [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users/' + item.id + '/posts')
+            .then(value => value.json())
+            .then(value => {
+                console.log(value);
+                setPosts(value);
+            });
+
+    }, [])
+    return (
+        <div>
+            <b>{item.name}</b>
+            <Posts items={posts}/>
+        </div>
+    )
+}
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,49 +1,51 @@
-import {useEffect, useState} from "react";
-import User from '../user/User';
-
-
-export default function Users () {
-    let [userslist, setUsersList] = useState([]);
-
-    useEffect(()=> {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(response => {
-                setUsersList(response);
-            });
-    }, []);
-
-    return (
-        <div>
-            {
-                userslist.map(value => <User item={value}/>)
-            }
-        </div>
-    )
-}
-
-////////////////////
 // import {useEffect, useState} from "react";
-// import User from "../user/User";
+// import User from '../user/User';
+//
 //
 // export default function Users () {
+//     let [userslist, setUsersList] = useState([]);
 //
-//     let [users, setUsers] = useState([]);
-//     useEffect(() => {
+//     useEffect(()=> {
 //         fetch('https://jsonplaceholder.typicode.com/users')
-//             .then(value => value.json())
-//             .then(value => {
-//                 // console.log(value)
-//                 setUsers(value);
+//             .then(response => response.json())
+//             .then(response => {
+//                 console.log(response);
+//                 setUsersList(response);
 //             });
-//
-//     }, [])
+//     }, []);
 //
 //     return (
 //         <div>
 //             {
-//                 users.map(value => <User item={value}/>)
+//                 userslist.map(value => <User item={value}/>)
 //             }
 //         </div>
-//     );
+//     )
 // }
+
+////////////////////
+////////////////////
+import {useEffect, useState} from "react";
+import User from "../user/User";
+
+export default function Users () {
+
+    let [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                // console.log(value)
+                setUsers(value);
+            });
+
+    }, [])
+
+    return (
+        <div>
+            {
+                users.map(value => <User item={value}/>)
+            }
+        </div>
+    );
+}
